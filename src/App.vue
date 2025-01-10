efb3bd4cfb85bbc54ac426b596aecb13a55e11c4
Badi1298 <serbandavid83@yahoo.com>
2025-01-10 10:50:48 +0200

    remove unused time tracking code; add About Us and Home pages with basic structure

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,4 @@
 
 <script setup>
 import { onMounted, onUnmounted } from 'vue';
-
-function trackPageTime(page, timeSpent) {
-	window.electronAPI
-		.trackTime(page, timeSpent)
-		.then((response) => console.log(response))
-		.catch((err) => console.error(err));
-}
-
-let startTime;
-
-onMounted(() => {
-	startTime = performance.now();
-
-	const handleBlur = () => {
-		const timeSpent = performance.now() - startTime;
-		trackPageTime('pageName', timeSpent / 1000); // Example usage
-	};
-
-	window.addEventListener('blur', handleBlur);
-
-	// Clean up the event listener on unmount
-	onUnmounted(() => {
-		window.removeEventListener('blur', handleBlur);
-	});
-});
 </script>
