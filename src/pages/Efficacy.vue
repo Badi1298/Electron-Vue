7292d0ec146d95d355574d2c0052b71a487029f2
Badi1298 <serbandavid83@yahoo.com>
2025-02-20 11:52:18 +0200

    add active and inactive dot images; enhance Efficacy page layout with new buttons and improved styling

--- src/pages/Efficacy.vue
+++ src/pages/Efficacy.vue
@@ -1,6 +1,18 @@
 <template>
-	<div class="pt-20">
-		<h1 class="text-[32px] text-electric-blue font-effra font-medium leading-normal max-w-[920px]">
+	<div class="pt-20 font-effra">
+		<div class="absolute left-[50px] top-1/2 -translate-y-1/2 flex flex-col gap-y-6">
+			<img
+				src="../assets/images/active-dot.png"
+				alt="Active Dot"
+				class="h-5 w-auto"
+			/>
+			<img
+				src="../assets/images/inactive-dot.png"
+				alt="Active Dot"
+				class="h-5 w-auto"
+			/>
+		</div>
+		<h1 class="text-[32px] text-electric-blue font-medium leading-normal max-w-[920px]">
 			EXBLIFEP® achieved superiority in overall treatment success* in patients with cUTIs vs piperacillin/tazobactam<sup>1</sup>
 		</h1>
 		<img
@@ -8,9 +20,20 @@
 			alt="Lime Green Border"
 			class="h-1.5 w-auto my-5"
 		/>
-		<p class="max-w-[1300px] text-xl font-effra leading-normal">
-			In ALLIUM, EXBLIFEP® demonstrated both non-inferiority and statistical superiority in overall treatment success (a composite endpoint of clinical
+		<p class="max-w-[1300px] text-xl leading-normal">
+			In ALLIUM, EXBLIFEP® demonstrated both non-inferiority and statistical superiority in overall treatment success (a composite endpoint of clinical
 			cure and microbiological eradication) at Day 14 vs piperacillin/tazobactam in patients in the PAS with cUTIs caused by gram-negative pathogens1
 		</p>
+
+		<div class="flex flex-col w-[1034px] pt-5">
+			<div class="grid grid-cols-[1fr_2fr] gap-x-2">
+				<button class="py-2.5 bg-electric-blue text-white rounded-t-[20px] text-2xl">Overall success*: Day 14</button>
+				<button class="py-2.5 bg-[#E4E4E4] rounded-t-[20px] text-2xl">Clinical cure & Microbiological eradication: Day 14</button>
+			</div>
+			<img
+				src="../assets/images/chart-a.svg"
+				alt="Chart A"
+			/>
+		</div>
 	</div>
 </template>
