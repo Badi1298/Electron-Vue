c0ab3f9379988b61b0f781d459db683a618bb776
Badi1298 <serbandavid83@yahoo.com>
2025-02-21 14:20:40 +0200

    add Dosing page tab components and images for step navigation

---
+++ src/components/exblifep/dosing/DosingBottomTab.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="grid grid-cols-1 grid-rows-1 min-h-screen">
+		<div
+			class="flex flex-col justify-end font-effra transition-all duration-300 relative pb-6"
+			:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+		>
+			<div>
+				<h1 class="text-[32px] font-bold text-electric-blue leading-normal max-w-[920px]">Reconstitution and dilution of EXBLIFEP<sup>®5</sup></h1>
+				<img
+					src="/src/assets/images/lime-green-border.png"
+					alt="Lime Green Border"
+					class="h-1.5 w-[300px] my-5"
+				/>
+			</div>
+		</div>
+		<footer class="relative pb-6">
+			<the-footer
+				class="transition-all duration-300 mb-4"
+				:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+				>EXBLIFEP® is not indicated in children as the safety and efficacy in children below 18 years of age has not yet been established. No data are
+				available.5</the-footer
+			>
+			<ExploreAnother />
+		</footer>
+	</div>
+</template>
+
+<script setup>
+import TheFooter from '../TheFooter.vue';
+import ExploreAnother from '../ExploreAnother.vue';
+
+defineProps({
+	sidebarOpen: {
+		type: Boolean,
+		required: true,
+	},
+});
+</script>
--- src/components/exblifep/dosing/DosingTopTab.vue
+++ src/components/exblifep/dosing/DosingTopTab.vue
@@ -1,15 +1,93 @@
 <template>
-	<div class="flex flex-col overflow-y-scroll">
-		<dosing-top-tab :sidebar-open="sidebarOpen" />
-		<dosing-bottom-tab :sidebar-open="sidebarOpen" />
+	<div class="grid grid-cols-1 grid-rows-1 min-h-screen">
+		<div
+			class="flex flex-col justify-end font-effra transition-all duration-300 relative pb-6"
+			:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+		>
+			<div class="flex flex-col gap-y-6 absolute top-1/2 left-[52px]">
+				<img
+					src="/src/assets/images/active-dot.png"
+					alt="Active Dot"
+					class="h-5 w-5 cursor-pointer"
+				/>
+				<img
+					src="/src/assets/images/inactive-dot.png"
+					alt="Active Dot"
+					class="h-5 w-5 cursor-pointer"
+				/>
+			</div>
+			<div class="flex justify-between">
+				<div>
+					<h1 class="text-[32px] font-bold text-electric-blue leading-normal max-w-[920px]">Dosing and administration of EXBLIFEP<sup>®5</sup></h1>
+					<img
+						src="/src/assets/images/lime-green-border.png"
+						alt="Lime Green Border"
+						class="h-1.5 w-[300px] my-5"
+					/>
+				</div>
+				<div class="flex items-center gap-3.5">
+					<img
+						src="/src/assets/images/touch.png"
+						alt="Touch to select tab"
+						class="w-[70px] h-[70px]"
+					/>
+					<span class="text-cool-grey text-2xl">Touch a step to continue</span>
+				</div>
+			</div>
+			<div class="flex pt-5 gap-x-16">
+				<div
+					v-for="tab in tabs"
+					:key="tab.id"
+					class="flex"
+					@click="activateTab(tab)"
+				>
+					<img
+						:src="tab.active ? tab.activeImageSrc : tab.inactiveImageSrc"
+						alt="Dosing Full"
+						class="h-[704px] w-auto z-20 cursor-pointer"
+					/>
+					<div
+						v-if="tab.active"
+						class="flex justify-center flex-col bg-white border-2 border-electric-blue pl-28 rounded-r-[30px] w-[806px] -ml-20"
+					>
+						<div class="flex items-end">
+							<img
+								src="/src/assets/images/dosing-bottle.png"
+								alt="Dosing Bottle"
+								class="w-[150px] h-[150px]"
+							/>
+							<h3 class="text-[40px] font-semibold text-electric-blue -translate-y-4 -translate-x-3">{{ tab.name }}</h3>
+						</div>
+						<div
+							class="flex flex-col pl-10 text-2xl text-cool-grey"
+							v-html="tab.details"
+						></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer class="relative pb-6">
+			<the-footer
+				class="transition-all duration-300 mb-4"
+				:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+				>EXBLIFEP® is not indicated in children as the safety and efficacy in children below 18 years of age has not yet been established. No data are
+				available.5</the-footer
+			>
+			<ExploreAnother />
+			<img
+				src="/src/assets/images/down-button-blue.png"
+				alt="Down Button"
+				class="w-[140px] h-[50px] absolute left-1/2 -translate-x-1/2 bottom-0 cursor-pointer"
+			/>
+		</footer>
 	</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-import DosingTopTab from '../components/exblifep/dosing/DosingTopTab.vue';
-import DosingBottomTab from '../components/exblifep/dosing/DosingBottomTab.vue';
+import TheFooter from '../TheFooter.vue';
+import ExploreAnother from '../ExploreAnother.vue';
 
 defineProps({
 	sidebarOpen: {
@@ -93,9 +171,3 @@ const activateTab = (tab) => {
 	tab.active = true;
 };
 </script>
-
-<style scoped>
-::-webkit-scrollbar {
-	display: none;
-}
-</style>
