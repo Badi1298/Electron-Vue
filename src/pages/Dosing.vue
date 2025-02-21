3764d811a351d3bddefd3d3d1106ea8e1224f181
Badi1298 <serbandavid83@yahoo.com>
2025-02-21 11:06:35 +0200

    add dosing images and enhance Dosing page layout with new components and styling

--- src/pages/Dosing.vue
+++ src/pages/Dosing.vue
@@ -1,3 +1,57 @@
 <template>
-	<h1>Dosing</h1>
+	<div class="grid grid-cols-1 grid-rows-1 pb-6">
+		<div
+			class="flex flex-col justify-end font-effra transition-all duration-300 relative pb-6"
+			:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+		>
+			<div class="flex flex-col gap-y-6 absolute top-1/2 left-[52px]">
+				<img
+					src="/src/assets/images/active-dot.png"
+					alt="Active Dot"
+					class="h-5 w-5 cursor-pointer"
+				/>
+				<img
+					src="/src/assets/images/inactive-dot.png"
+					alt="Active Dot"
+					class="h-5 w-5 cursor-pointer"
+				/>
+			</div>
+			<div>
+				<h1 class="text-[32px] font-bold text-electric-blue leading-normal max-w-[920px]">Dosing and administration of EXBLIFEP<sup>®5</sup></h1>
+				<img
+					src="/src/assets/images/lime-green-border.png"
+					alt="Lime Green Border"
+					class="h-1.5 w-[300px] my-5"
+				/>
+			</div>
+			<div class="pt-5">
+				<img
+					src="/src/assets/images/dosing-button-full.png"
+					alt="Dosing Full"
+					class="h-[704px] w-auto"
+				/>
+			</div>
+		</div>
+		<footer>
+			<the-footer
+				class="transition-all duration-300 mb-4"
+				:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+				>EXBLIFEP® is not indicated in children as the safety and efficacy in children below 18 years of age has not yet been established. No data are
+				available.5</the-footer
+			>
+			<ExploreAnother />
+		</footer>
+	</div>
 </template>
+
+<script setup>
+import TheFooter from '../components/exblifep/TheFooter.vue';
+import ExploreAnother from '../components/exblifep/ExploreAnother.vue';
+
+defineProps({
+	sidebarOpen: {
+		type: Boolean,
+		required: true,
+	},
+});
+</script>
