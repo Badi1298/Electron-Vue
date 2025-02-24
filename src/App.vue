4ad27a77e3a7dd01f902a580a0cc9179922f5f4d
Badi1298 <serbandavid83@yahoo.com>
2025-02-24 17:48:45 +0200

    feat: add screensaver functionality with inactivity detection and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
-	<router-view v-slot="{ Component }">
-		<!-- <transition name="fade"> -->
-		<component :is="Component" />
-		<!-- </transition> -->
-	</router-view>
+	<div id="app">
+		<router-view v-slot="{ Component }">
+			<!-- <transition name="fade"> -->
+			<component :is="Component" />
+			<!-- </transition> -->
+		</router-view>
+		<div
+			v-if="showScreensaver"
+			id="screensaver"
+			@click="hideScreensaver"
+			@mousemove="hideScreensaver"
+			class="fixed top-0 left-0 w-full h-full z-50 bg-black/100 cursor-pointer flex justify-center items-center"
+		>
+			<div class="bg-lightblue-500 w-full h-full flex justify-center items-center">
+				<h1 class="text-4xl font-bold text-white">Screensaver</h1>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
+import { ref, onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const showScreensaver = ref(false);
+let inactivityTimer;
+
+const resetInactivityTimer = () => {
+	clearTimeout(inactivityTimer);
+	showScreensaver.value = false;
+	inactivityTimer = setTimeout(() => {
+		showScreensaver.value = true;
+	}, 20000);
+};
+
+const hideScreensaver = () => {
+	showScreensaver.value = false;
+	resetInactivityTimer();
+	router.push('/');
+};
+
+onMounted(() => {
+	resetInactivityTimer();
+});
+
+onUnmounted(() => {
+	clearTimeout(inactivityTimer);
+});
 </script>
 
 <style scoped>
