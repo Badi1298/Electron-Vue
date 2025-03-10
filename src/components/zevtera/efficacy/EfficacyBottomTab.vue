6c59c0e93a1e1ef80ea7092ac495eecfff4e9370
Badi1298 <serbandavid83@yahoo.com>
2025-03-10 17:48:16 +0200

    feat: add new pathogen images and enhance EfficacyBottomTab component layout for improved user experience

--- src/components/zevtera/efficacy/EfficacyBottomTab.vue
+++ src/components/zevtera/efficacy/EfficacyBottomTab.vue
@@ -25,14 +25,58 @@
 				alt="Bullet Long"
 				class="-translate-x-20"
 			/>
-			<the-title>More pathogens covered</the-title>
+			<div class="grid grid-cols-2 gap-x-28 pr-[60px] items-center">
+				<div>
+					<the-title>More pathogens covered</the-title>
+					<div class="flex items-center gap-x-5 bg-primary-green px-6 py-5 rounded-[20px] shadow-zevtera-efficacy-pathogens-card mt-8">
+						<img
+							src="/src/assets/images/tick-icon-dark-green.png"
+							alt="Tick"
+							class="w-[82px] h-auto"
+						/>
+						<h4 class="text-[32px] font-bold text-white">Documented use<sup>3</sup></h4>
+					</div>
+					<div class="flex items-center gap-x-5 bg-primary-green px-6 py-5 rounded-[20px] shadow-zevtera-efficacy-pathogens-card mt-[18px]">
+						<img
+							src="/src/assets/images/tick-icon-dark-green.png"
+							alt="Tick"
+							class="w-[82px] h-auto"
+						/>
+						<h4 class="text-[32px] font-bold text-white">Empiric use<sup>2,7,8</sup></h4>
+					</div>
+					<div class="flex items-center gap-x-5 bg-primary-green px-6 py-5 rounded-[20px] shadow-zevtera-efficacy-pathogens-card mt-[18px]">
+						<img
+							src="/src/assets/images/tick-icon-dark-green.png"
+							alt="Tick"
+							class="w-[82px] h-auto"
+						/>
+						<h4 class="text-[32px] font-bold text-white">Adapted to your local stewardship program<sup>4,8</sup></h4>
+					</div>
+				</div>
+				<div class="relative z-50 flex flex-col bg-white p-12 rounded-[20px] border-primary-green border-[5px]">
+					<h3 class="font-bold text-2xl leading-normal text-center">
+						Early improvement at Day 4 in patients with HAP (excluding VAP) by pathogen type(CE)<sup>6</sup>
+					</h3>
+					<VLazyImage
+						:src="PathogensChart"
+						alt="Pathogens Chart"
+						class="w-[503px] self-end mr-8"
+					/>
+					<p class="text-[10px] text-[#555] text-center mt-8">Adapted from Scheeren T et al. 2019.6</p>
+					<img
+						src="/src/assets/images/expand-icon-purple.png"
+						alt="Expand Icon"
+						class="absolute bottom-5 right-7 w-[60px] h-[60px] cursor-pointer"
+					/>
+				</div>
+			</div>
 		</div>
 
 		<footer>
 			<div class="flex justify-between items-center mt-4 mr-12">
 				<ExploreAnother />
 				<RouterLink :to="{ name: 'exblifep-safety', query: { navigatedAwayBy: 'next-section-button' } }">
-					<next-section class="bg-electric-blue" />
+					<next-section class="bg-primary-purple" />
 				</RouterLink>
 			</div>
 		</footer>
@@ -42,15 +86,15 @@
 <script setup>
 import { ref, watch } from 'vue';
 
+import VLazyImage from 'v-lazy-image';
+
+import { gsap } from 'gsap';
+
+import PathogensChart from '@/assets/images/pathogens-chart.png';
+
 import NextSection from '@/components/NextSection.vue';
 import TheTitle from '@/components/zevtera/TheTitle.vue';
 import ExploreAnother from '@/components/ExploreAnother.vue';
-
-const Tabs = Object.freeze({
-	DROP: 1,
-	TIME: 2,
-	LUNGS: 3,
-});
 
 const props = defineProps({
 	sidebarOpen: {
@@ -75,27 +119,6 @@ watch(
 		}
 	}
 );
-
-const tabsInfo = ref([
-	{
-		id: Tabs.DROP,
-		active: true,
-	},
-	{
-		id: Tabs.TIME,
-		active: false,
-	},
-	{
-		id: Tabs.LUNGS,
-		active: false,
-	},
-]);
-
-const onClickTab = (id) => {
-	tabsInfo.value.forEach((tab) => {
-		tab.active = tab.id === id;
-	});
-};
 </script>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,8 @@ export default {
 				dark: '3px 4px 10px 2px rgba(0, 0, 0, 0.25)',
 				'zevtera-efficacy-card':
 					'-39px 347px 98px 0px rgba(0, 0, 0, 0.00), -25px 222px 89px 0px rgba(0, 0, 0, 0.01), -14px 125px 75px 0px rgba(0, 0, 0, 0.05), -6px 56px 56px 0px rgba(0, 0, 0, 0.09), -2px 14px 31px 0px rgba(0, 0, 0, 0.10)',
+				'zevtera-efficacy-pathogens-card':
+					'-49px 114px 35px 0px rgba(0, 0, 0, 0.00), -31px 73px 32px 0px rgba(0, 0, 0, 0.01), -18px 41px 27px 0px rgba(0, 0, 0, 0.05), -8px 18px 20px 0px rgba(0, 0, 0, 0.09), -2px 5px 11px 0px rgba(0, 0, 0, 0.10)',
 			},
 		},
 	},
