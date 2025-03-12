ce65067d0c60c58aa159f2f14334775f203e4273
Badi1298 <serbandavid83@yahoo.com>
2025-03-12 17:59:11 +0200

    feat: add SafetyTopTab and SafetyBottomTab components for enhanced safety information display

---
+++ src/components/xyd/safety/SafetyTopTab.vue
@@ -0,0 +1,109 @@
+<template>
+	<div
+		ref="topTab"
+		class="grid grid-cols-1 grid-rows-1 min-h-screen relative z-10"
+	>
+		<div class="flex flex-col gap-y-6 absolute top-1/2 left-[52px]">
+			<img
+				src="/src/assets/images/active-dot-purple.png"
+				alt="Active Dot"
+				class="h-5 w-5 cursor-pointer"
+			/>
+			<img
+				src="/src/assets/images/inactive-dot.png"
+				alt="Active Dot"
+				class="h-5 w-5 cursor-pointer"
+				@click="emit('goToBottomTab')"
+			/>
+		</div>
+		<div
+			class="flex flex-col justify-end font-effra transition-all duration-300"
+			:class="[sidebarOpen ? 'ml-[124px]' : 'ml-[224px]']"
+		>
+			<h1 class="text-[40px] font-bold text-primary-purple">Xydalba™ demonstrated clinical success at Day 14-15<sup>8</sup></h1>
+
+			<section class="grid grid-cols-[1fr_0.34fr] items-end gap-x-6 mt-7 pr-[60px]">
+				<div>
+					<div class="bg-primary-green py-7 rounded-t-[20px] pl-10 text-xl font-bold text-white">
+						Xydalba™ provides clinical success at end-of-treatment similar to vancomycin/linezolid<sup>8</sup>
+					</div>
+					<div class="bg-white px-16 py-6 shadow-treatment">
+						<VLazyImage
+							:src="Chart"
+							alt="Chart 1"
+						/>
+					</div>
+				</div>
+				<div class="flex flex-col gap-y-8 items-center justify-center px-3.5 border-2 border-primary-purple rounded-r-[20px] h-[587px]">
+					<img
+						src="/src/assets/images/efficacy-purple.png"
+						alt="Clock"
+						class="w-[90px] h-[90px] mx-auto"
+					/>
+					<p class="text-[22px] text-primary-purple text-center">
+						Xydalba™ delivers the clinical success your patients need, with
+						<span class="font-bold">91% achieving relief from symptoms at two weeks,</span> in patients infected with S. aureus (including MRSA)*8
+					</p>
+				</div>
+			</section>
+		</div>
+
+		<footer class="relative pb-6 pt-8">
+			<the-footer
+				class="transition-all duration-300 mb-4"
+				:class="[sidebarOpen ? 'pl-[124px]' : 'pl-[224px]']"
+				>*EMA primary end-point: Clinical success at End-of-treatment visit (Day 14-15) in the ITT population defined as decreased lesion size (both
+				length and width measurements), a temperature of ≤37.6°C, local signs of fluctuance and localized heat/warmth were absent; local signs of<br />
+				tenderness to palpation and swelling/induration were no worse than mild; and for patients with a wound infection, the severity of purulent
+				drainage was improved and no worse than mild relative to baseline; no need for further systemic antibacterial treatment for the SSTI.<br />
+				ITT: Intent To Treat.</the-footer
+			>
+			<ExploreAnother />
+			<button
+				@click="emit('goToBottomTab')"
+				class="absolute left-1/2 -translate-x-1/2 bottom-0"
+			>
+				<img
+					src="/src/assets/images/page-vertical-down-green.png"
+					alt="Down Button"
+					class="w-[140px] h-[50px] cursor-pointer"
+				/>
+			</button>
+		</footer>
+	</div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+import VLazyImage from 'v-lazy-image';
+
+import Chart from '@/assets/images/xyd-treatment-chart-1.svg';
+
+import TheFooter from '@/components/TheFooter.vue';
+import ExploreAnother from '@/components/ExploreAnother.vue';
+
+const props = defineProps({
+	sidebarOpen: {
+		type: Boolean,
+		required: true,
+	},
+	scrollIntoView: {
+		type: Boolean,
+		required: true,
+	},
+});
+
+const emit = defineEmits(['goToBottomTab']);
+
+const topTab = ref(null);
+
+watch(
+	() => props.scrollIntoView,
+	(value) => {
+		if (value) {
+			topTab.value.scrollIntoView({ behavior: 'smooth' });
+		}
+	}
+);
+</script>
--- src/pages/xyd/Safety.vue
+++ src/pages/xyd/Safety.vue
@@ -1,3 +1,51 @@
 <template>
-	<h1>Safety</h1>
+	<div class="overflow-y-scroll">
+		<safety-top-tab
+			:sidebarOpen="sidebarOpen"
+			:scroll-into-view="scrollToTopTab"
+			@go-to-bottom-tab="onScrollToBottomTab"
+		/>
+		<safety-bottom-tab
+			:sidebar-open="sidebarOpen"
+			:scroll-into-view="scrollToBottomTab"
+			@go-to-top-tab="onScrollToTopTab"
+		/>
+	</div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+import SafetyTopTab from '@/components/xyd/safety/SafetyTopTab.vue';
+import SafetyBottomTab from '@/components/xyd/safety/SafetyBottomTab.vue';
+
+defineProps({
+	sidebarOpen: {
+		type: Boolean,
+		required: true,
+	},
+});
+
+const scrollToTopTab = ref(false);
+const scrollToBottomTab = ref(false);
+
+const onScrollToTopTab = () => {
+	scrollToTopTab.value = true;
+	setTimeout(() => {
+		scrollToTopTab.value = false;
+	}, 1000);
+};
+
+const onScrollToBottomTab = () => {
+	scrollToBottomTab.value = true;
+	setTimeout(() => {
+		scrollToBottomTab.value = false;
+	}, 1000);
+};
+</script>
+
+<style scoped>
+::-webkit-scrollbar {
+	display: none;
+}
+</style>
