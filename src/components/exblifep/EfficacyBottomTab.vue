a3f5c04697855d9b10805c796d9a84528149a825
Badi1298 <serbandavid83@yahoo.com>
2025-03-17 21:48:36 +0200

    feat: enhance EfficacyBottomTab layout with improved spacing and updated content presentation

--- src/components/exblifep/EfficacyBottomTab.vue
+++ src/components/exblifep/EfficacyBottomTab.vue
@@ -17,7 +17,7 @@
 			/>
 		</div>
 		<div
-			class="flex flex-col justify-center font-effra transition-all duration-300 relative"
+			class="flex flex-col justify-center font-effra transition-all duration-300 relative pr-[60px]"
 			:class="[sidebarOpen ? 'ml-[124px]' : 'ml-[224px]']"
 		>
 			<div class="flex justify-between mr-12 items-center">
@@ -32,161 +32,70 @@
 						class="h-1.5 w-[300px] my-5"
 					/>
 				</div>
-				<div
-					class="flex gap-x-3.5 items-center text-cool-grey text-2xl font-medium transform transition-all duration-300"
-					:class="[sidebarOpen ? 'translate-x-0' : '-translate-x-full']"
-				>
-					<img
-						src="/src/assets/images/touch.png"
-						alt="Touch to select tab"
-						class="w-[70px] h-[70px]"
-					/>
-					<span>Select a tab</span>
-				</div>
 			</div>
 
-			<section class="flex gap-x-16">
-				<div class="flex flex-col gap-y-12">
-					<div
-						class="relative w-fit cursor-pointer"
-						@click="onClickTab(Tabs.DROP)"
-					>
-						<img
-							src="/src/assets/images/water-drop.png"
-							alt="Water Drop"
-							class="w-[130px] h-[130px] z-10 relative"
-						/>
-						<img
-							v-if="tabsInfo[0].active"
-							src="/src/assets/images/active-arrow.png"
-							alt="Active Arrow"
-							class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
-						/>
-						<img
-							v-else
-							src="/src/assets/images/touch.png"
-							alt="Touch"
-							class="w-[40px] h-[40px] absolute top-1/2 -right-5 transform -translate-y-1/2 z-20"
-						/>
+			<section class="flex gap-x-12 mt-14 items-center">
+				<div class="flex flex-col gap-y-9">
+					<div class="flex items-center">
+						<div class="relative w-fit cursor-pointer">
+							<img
+								src="/src/assets/images/water-drop.png"
+								alt="Water Drop"
+								class="min-w-[130px] max-w-[130px] z-10 relative"
+							/>
+							<img
+								src="/src/assets/images/active-arrow.png"
+								alt="Active Arrow"
+								class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
+							/>
+						</div>
+						<div class="relative bg-white border-2 border-electric-blue max-w-[680px] ml-16 rounded-[20px] px-10 py-10 overflow-hidden">
+							<h4 class="text-2xl font-medium text-cool-grey">
+								Cefepime and enmetazobactam have demonstrated similar concentration-time profiles in plasma and ELF.<sup>6</sup>
+							</h4>
+						</div>
 					</div>
-					<div
-						class="relative w-fit cursor-pointer"
-						@click="onClickTab(Tabs.TIME)"
-					>
-						<img
-							src="/src/assets/images/24-hours.png"
-							alt="24 Hours"
-							class="w-[130px] h-[130px] z-10 relative"
-						/>
-						<img
-							v-if="tabsInfo[1].active"
-							src="/src/assets/images/active-arrow.png"
-							alt="Active Arrow"
-							class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
-						/>
-						<img
-							v-else
-							src="/src/assets/images/touch.png"
-							alt="Touch"
-							class="w-[40px] h-[40px] absolute top-1/2 -right-5 transform -translate-y-1/2 z-20"
-						/>
+					<div class="flex items-center">
+						<div class="relative w-fit cursor-pointer">
+							<img
+								src="/src/assets/images/water-drop.png"
+								alt="Water Drop"
+								class="min-w-[130px] max-w-[130px] z-10 relative"
+							/>
+							<img
+								src="/src/assets/images/active-arrow.png"
+								alt="Active Arrow"
+								class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
+							/>
+						</div>
+						<div class="relative bg-white border-2 border-electric-blue max-w-[680px] ml-16 rounded-[20px] px-10 py-10 overflow-hidden">
+							<h4 class="text-2xl font-medium text-cool-grey">
+								Cefepime and enmetazobactam have demonstrated similar concentration-time profiles in plasma and ELF.<sup>6</sup>
+							</h4>
+						</div>
 					</div>
-					<div
-						class="relative w-fit cursor-pointer"
-						@click="onClickTab(Tabs.LUNGS)"
-					>
-						<img
-							src="/src/assets/images/lungs.png"
-							alt="Lungs"
-							class="w-[130px] h-[130px] z-10 relative"
-						/>
-						<img
-							v-if="tabsInfo[2].active"
-							src="/src/assets/images/active-arrow.png"
-							alt="Active Arrow"
-							class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
-						/>
-						<img
-							v-else
-							src="/src/assets/images/touch.png"
-							alt="Touch"
-							class="w-[40px] h-[40px] absolute top-1/2 -right-5 transform -translate-y-1/2 z-20"
-						/>
+					<div class="flex items-center">
+						<div class="relative w-fit cursor-pointer">
+							<img
+								src="/src/assets/images/water-drop.png"
+								alt="Water Drop"
+								class="min-w-[130px] max-w-[130px] z-10 relative"
+							/>
+							<img
+								src="/src/assets/images/active-arrow.png"
+								alt="Active Arrow"
+								class="w-[70px] h-[70px] absolute top-1/2 -right-[52px] transform -translate-y-1/2 z-0"
+							/>
+						</div>
+						<div class="relative bg-white border-2 border-electric-blue max-w-[680px] ml-16 rounded-[20px] px-10 py-10 overflow-hidden">
+							<h4 class="text-2xl font-medium text-cool-grey">
+								Cefepime and enmetazobactam have demonstrated similar concentration-time profiles in plasma and ELF.<sup>6</sup>
+							</h4>
+						</div>
 					</div>
 				</div>
-				<Transition
-					name="fade"
-					mode="out-in"
-				>
-					<div
-						v-if="tabsInfo[0].active"
-						class="relative bg-white border border-electric-blue max-w-[474px] rounded-[20px] px-10 py-14 overflow-hidden"
-					>
-						<h4 class="text-2xl font-medium text-cool-grey">
-							Cefepime and enmetazobactam have demonstrated similar concentration-time profiles in plasma and ELF.<sup>6</sup>
-						</h4>
-						<img
-							src="/src/assets/images/lime-green-border.png"
-							alt="Lime Green Border"
-							class="h-1.5 w-auto mt-5"
-						/>
-						<img
-							src="/src/assets/images/background-green-circle.png"
-							alt="Green Circle"
-							class="absolute bottom-0 -right-[130px] z-0"
-						/>
-						<img
-							src="/src/assets/images/water-drop.png"
-							class="w-28 h-28 absolute bottom-3 right-6 z-10"
-						/>
-					</div>
-					<div
-						v-else-if="tabsInfo[1].active"
-						class="relative bg-white border border-electric-blue max-w-[474px] rounded-[20px] px-10 py-14 overflow-hidden"
-					>
-						<h4 class="text-2xl font-medium text-cool-grey">
-							Concentrations of both agents are detectable in plasma for 24 hours after last administration.<sup>6</sup>
-						</h4>
-						<img
-							src="/src/assets/images/lime-green-border.png"
-							alt="Lime Green Border"
-							class="h-1.5 w-auto mt-5"
-						/>
-						<img
-							src="/src/assets/images/background-green-circle.png"
-							alt="Green Circle"
-							class="absolute bottom-0 -right-[130px] z-0"
-						/>
-						<img
-							src="/src/assets/images/24-hours.png"
-							class="w-28 h-28 absolute bottom-3 right-6 z-10"
-						/>
-					</div>
-					<div
-						v-else-if="tabsInfo[2].active"
-						class="relative bg-white border border-electric-blue max-w-[474px] rounded-[20px] px-10 py-14 overflow-hidden"
-					>
-						<h4 class="text-2xl font-medium text-cool-grey">
-							Cefepime has high lung tissue penetration compared with other cephalosporins, and is recommended in European clinical guidelines for
-							the management of HAP/VAP.<sup>7,8</sup>
-						</h4>
-						<img
-							src="/src/assets/images/lime-green-border.png"
-							alt="Lime Green Border"
-							class="h-1.5 w-auto mt-5"
-						/>
-						<img
-							src="/src/assets/images/background-green-circle.png"
-							alt="Green Circle"
-							class="absolute bottom-0 -right-[130px] z-0"
-						/>
-						<img
-							src="/src/assets/images/lungs.png"
-							class="w-28 h-28 absolute bottom-3 right-6 z-10"
-						/>
-					</div>
-				</Transition>
-				<div class="pt-14 max-w-[624px]">
+
+				<div class="max-w-[416px]">
 					<h4 class="text-2xl font-bold leading-normal text-[#002470]">In an intrapulmonary PK study of 20 healthy volunteers:<sup>*6</sup></h4>
 					<p class="text-xl leading-normal font-medium text-cool-grey mt-4">
 						Concentration-time of cefepime and enmetazobactam in plasma and epithelial lining fluid were similar
@@ -203,18 +112,6 @@
 						enmetazobactam and 46.8% for cefepime were obtained.<sup>5</sup>
 					</p>
 				</div>
-			</section>
-			<section class="bg-[#00EAFF] py-4 flex items-center mt-9 relative z-10 border-[6px] border-white max-w-[1400px]">
-				<div class="pl-9 py-3 border-r-[3px] border-[#002470]">
-					<img
-						src="/src/assets/images/chevron-right-bleu.png"
-						alt="Chevron Right Bleu"
-						class="w-[62px] h-auto mr-7"
-					/>
-				</div>
-				<h2 class="pl-10 text-2xl font-bold text-[#002470]">
-					EXBLIFEP® is indicated to treat HAP/VAP, and could be a useful carbapenem-sparing option for ESBL-producing pathogens<sup>†5,9</sup>
-				</h2>
 			</section>
 		</div>
 
