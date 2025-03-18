af80b9cf7de6d7bfd2e9f8c984d04e03fb6c6076
Badi1298 <serbandavid83@yahoo.com>
2025-03-18 13:50:34 +0200

    feat: improve layout of ExploreAnother component and enhance sidebar transition animations

--- src/components/zevtera/Sidebar.vue
+++ src/components/zevtera/Sidebar.vue
@@ -1,201 +1,217 @@
 <template>
-	<aside
-		:class="[open ? 'w-[350px] grid-rows-[0.5fr_3fr_1fr]' : 'w-[118px] mt-48']"
-		class="z-10 grid my-8 bg-white rounded-l-[20px] pt-12 transition-all duration-300 relative shadow-sidebar"
-	>
-		<div v-if="open"></div>
-		<ul class="flex flex-col gap-y-4 px-4 self-center">
-			<RouterLink :to="{ name: 'zevtera-home', query: { navigatedAwayBy: 'sidebar' } }">
-				<li
-					class="px-3 py-5 font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
-					:class="{ 'justify-center': !open, 'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera'), 'text-[#969696]': !isActive('/zevtera') }"
-				>
-					<img
-						v-if="isActive('/zevtera')"
-						src="/src/assets/images/home-sidebar-full.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<img
-						v-else
-						src="/src/assets/images/home-sidebar.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<p class="opacity-0 hidden sidebar-text">Home</p>
-				</li>
-			</RouterLink>
-			<RouterLink :to="{ name: 'zevtera-efficacy', query: { navigatedAwayBy: 'sidebar' } }">
-				<li
-					class="px-3 py-5 font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
-					:class="{
-						'justify-center': !open,
-						'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/efficacy'),
-						'text-[#969696]': !isActive('/zevtera/efficacy'),
-					}"
-				>
-					<img
-						v-if="isActive('/zevtera/efficacy')"
-						src="/src/assets/images/chart-sidebar-full.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<img
-						v-else
-						src="/src/assets/images/chart-sidebar.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<p class="opacity-0 hidden sidebar-text">Efficacy</p>
-				</li>
-			</RouterLink>
-			<RouterLink :to="{ name: 'zevtera-safety', query: { navigatedAwayBy: 'sidebar' } }">
-				<li
-					class="px-3 py-5 font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
-					:class="{
-						'justify-center': !open,
-						'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/safety'),
-						'text-[#969696]': !isActive('/zevtera/safety'),
-					}"
-				>
-					<img
-						v-if="isActive('/zevtera/safety')"
-						src="/src/assets/images/shield-sidebar-full.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<img
-						v-else
-						src="/src/assets/images/shield-sidebar.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<p class="opacity-0 hidden sidebar-text">Safety</p>
-				</li>
-			</RouterLink>
-			<RouterLink :to="{ name: 'zevtera-dosing', query: { navigatedAwayBy: 'sidebar' } }">
-				<li
-					class="px-3 py-5 font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
-					:class="{
-						'justify-center': !open,
-						'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/dosing'),
-						'text-[#969696]': !isActive('/zevtera/dosing'),
-					}"
-				>
-					<img
-						v-if="isActive('/zevtera/dosing')"
-						src="/src/assets/images/bottle-sidebar-full.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<img
-						v-else
-						src="/src/assets/images/bottle-sidebar.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<p class="opacity-0 hidden sidebar-text">Dosing</p>
-				</li>
-			</RouterLink>
-			<RouterLink :to="{ name: 'zevtera-summary', query: { navigatedAwayBy: 'sidebar' } }">
-				<li
-					class="px-3 py-5 font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
-					:class="{
-						'justify-center': !open,
-						'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/summary'),
-						'text-[#969696]': !isActive('/zevtera/summary'),
-					}"
-				>
-					<img
-						v-if="isActive('/zevtera/summary')"
-						src="/src/assets/images/summary-sidebar-full.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<img
-						v-else
-						src="/src/assets/images/summary-sidebar.png"
-						alt="Home Icon"
-						class="w-[30px] h-[30px]"
-					/>
-					<p class="opacity-0 hidden sidebar-text">Summary</p>
-				</li>
-			</RouterLink>
-		</ul>
-		<div class="flex flex-col">
+	<div class="flex">
+		<div class="flex items-center absolute z-20 top-[17%] w-full">
 			<div
-				class="grid items-center"
-				:class="[open ? 'grid-cols-2' : 'grid-cols-1 gap-y-10']"
+				class="relative transform w-full"
+				@click="toggleSidebar"
 			>
-				<RouterLink :to="{ name: 'zevtera-references', query: { navigatedAwayBy: 'sidebar' } }">
-					<img
-						src="/src/assets/images/book.png"
-						alt="Book Icon"
-						class="w-8 h-8 justify-self-center"
-						@click="referencesPopupOpen = true"
+				<button class="absolute top-1/2 -translate-y-1/2 -left-5 bg-[#ECECEC] text-white p-2.5 rounded-md">
+					<SimpleChevronRightIcon
+						v-if="open"
+						class="w-6 h-6"
 					/>
-				</RouterLink>
-				<RouterLink :to="{ name: 'zevtera-prescribing-information', query: { navigatedAwayBy: 'sidebar' } }">
-					<img
-						src="/src/assets/images/pi.png"
-						alt="PI Icon"
-						class="w-4 h-auto justify-self-center"
-						@click="prescribingPopupOpen = true"
+					<SimpleChevronLeftIcon
+						v-else
+						class="w-6 h-6"
 					/>
-				</RouterLink>
+				</button>
+				<div
+					ref="sidebarLine"
+					class="bg-[#CDCDCD] absolute top-1/2 -translate-y-1/2 left-6 h-px w-full -z-10"
+				></div>
 			</div>
-			<div
-				v-if="open"
-				class="grid grid-cols-2 text-center"
-			>
-				<RouterLink :to="{ name: 'zevtera-references', query: { navigatedAwayBy: 'sidebar' } }">
-					<p
-						class="text-xl font-uni-grotesk text-[#969696]"
-						@click="referencesPopupOpen = true"
-					>
-						References
-					</p>
-				</RouterLink>
-				<RouterLink :to="{ name: 'zevtera-prescribing-information', query: { navigatedAwayBy: 'sidebar' } }">
-					<p
-						class="text-xl font-uni-grotesk text-[#969696]"
-						@click="prescribingPopupOpen = true"
-					>
-						Prescribing information
-					</p>
-				</RouterLink>
-			</div>
-			<img
-				v-if="open"
-				src="/src/assets/images/advanz-logo.png"
-				alt="Advanz Logo"
-				:class="[open ? 'w-44' : 'w-12']"
-				class="h-auto m-auto"
-			/>
-			<img
-				v-else
-				src="/src/assets/images/advanz-logo-small.png"
-				alt="Advanz Logo"
-				:class="[open ? 'w-44' : 'w-12']"
-				class="h-auto m-auto"
-			/>
 		</div>
-
-		<button
-			class="absolute -left-5 transform bg-gray-700 text-white p-2.5 rounded-md bg-[#ECECEC]"
-			:class="[open ? 'top-[140px]' : '-top-5']"
-			@click="toggleSidebar"
+		<aside
+			ref="sidebar"
+			class="z-10 flex flex-col mb-8 bg-white rounded-l-[20px] transition-all relative shadow-sidebar"
 		>
-			<SimpleChevronRightIcon
-				v-if="open"
-				class="w-6 h-6"
-			/>
-			<SimpleChevronLeftIcon
-				v-else
-				class="w-6 h-6"
-			/>
-		</button>
-	</aside>
+			<div class="relative flex flex-col flex-1 justify-end mb-20">
+				<ul class="flex flex-col gap-y-4 px-4">
+					<RouterLink :to="{ name: 'zevtera-home', query: { navigatedAwayBy: 'sidebar' } }">
+						<li
+							class="list__item relative px-3 py-5 min-h-[70px] font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
+							:class="{
+								'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera'),
+								'text-[#969696]': !isActive('/zevtera'),
+							}"
+						>
+							<div class="list-image absolute top-1/2 -translate-y-1/2">
+								<img
+									v-if="isActive('/zevtera')"
+									src="/src/assets/images/home-sidebar-full.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+								<img
+									v-else
+									src="/src/assets/images/home-sidebar.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+							</div>
+							<p class="sidebar-text absolute left-[52px] top-1/2 -translate-y-1/2">Home</p>
+						</li>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-efficacy', query: { navigatedAwayBy: 'sidebar' } }">
+						<li
+							class="list__item relative px-3 py-5 min-h-[70px] font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
+							:class="{
+								'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/efficacy'),
+								'text-[#969696]': !isActive('/zevtera/efficacy'),
+							}"
+						>
+							<div class="list-image absolute top-1/2 -translate-y-1/2">
+								<img
+									v-if="isActive('/zevtera/efficacy')"
+									src="/src/assets/images/chart-sidebar-full.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+								<img
+									v-else
+									src="/src/assets/images/chart-sidebar.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+							</div>
+							<p class="sidebar-text absolute left-[52px] top-1/2 -translate-y-1/2">Efficacy</p>
+						</li>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-safety', query: { navigatedAwayBy: 'sidebar' } }">
+						<li
+							class="list__item relative px-3 py-5 min-h-[70px] font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
+							:class="{
+								'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/safety'),
+								'text-[#969696]': !isActive('/zevtera/safety'),
+							}"
+						>
+							<div class="list-image absolute top-1/2 -translate-y-1/2">
+								<img
+									v-if="isActive('/zevtera/safety')"
+									src="/src/assets/images/shield-sidebar-full.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+								<img
+									v-else
+									src="/src/assets/images/shield-sidebar.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+							</div>
+							<p class="sidebar-text absolute left-[52px] top-1/2 -translate-y-1/2">Safety</p>
+						</li>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-dosing', query: { navigatedAwayBy: 'sidebar' } }">
+						<li
+							class="list__item relative px-3 py-5 min-h-[70px] font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
+							:class="{
+								'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/dosing'),
+								'text-[#969696]': !isActive('/zevtera/dosing'),
+							}"
+						>
+							<div class="list-image absolute top-1/2 -translate-y-1/2">
+								<img
+									v-if="isActive('/zevtera/dosing')"
+									src="/src/assets/images/bottle-sidebar-full.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+								<img
+									v-else
+									src="/src/assets/images/bottle-sidebar.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+							</div>
+							<p class="sidebar-text absolute left-[52px] top-1/2 -translate-y-1/2">Dosing</p>
+						</li>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-summary', query: { navigatedAwayBy: 'sidebar' } }">
+						<li
+							class="list__item relative px-3 py-5 min-h-[70px] font-uni-grotesk text-xl leading-normal rounded-md flex gap-x-2.5 items-center"
+							:class="{
+								'bg-[#EFEFEF] font-bold text-black': isActive('/zevtera/summary'),
+								'text-[#969696]': !isActive('/zevtera/summary'),
+							}"
+						>
+							<div class="list-image absolute top-1/2 -translate-y-1/2">
+								<img
+									v-if="isActive('/zevtera/summary')"
+									src="/src/assets/images/summary-sidebar-full.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+								<img
+									v-else
+									src="/src/assets/images/summary-sidebar.png"
+									alt="Home Icon"
+									class="w-[30px] h-[30px]"
+								/>
+							</div>
+							<p class="sidebar-text absolute left-[52px] top-1/2 -translate-y-1/2">Summary</p>
+						</li>
+					</RouterLink>
+				</ul>
+			</div>
+			<div class="flex flex-col mx-4 py-2.5 border-t border-[#CDCDCD] min-h-[240px]">
+				<div
+					ref="bottomIcons"
+					class="grid grid-cols-2 gap-y-9 items-center"
+				>
+					<RouterLink :to="{ name: 'zevtera-references', query: { navigatedAwayBy: 'sidebar' } }">
+						<img
+							src="/src/assets/images/book.png"
+							alt="Book Icon"
+							class="w-8 h-8 justify-self-center"
+							@click="referencesPopupOpen = true"
+						/>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-prescribing-information', query: { navigatedAwayBy: 'sidebar' } }">
+						<img
+							src="/src/assets/images/pi.png"
+							alt="PI Icon"
+							class="w-[30px] h-auto justify-self-center"
+							@click="prescribingPopupOpen = true"
+						/>
+					</RouterLink>
+				</div>
+				<div
+					ref="bottomText"
+					class="grid grid-cols-2 text-center"
+				>
+					<RouterLink :to="{ name: 'zevtera-references', query: { navigatedAwayBy: 'sidebar' } }">
+						<p
+							class="text-xl font-uni-grotesk text-[#969696]"
+							@click="referencesPopupOpen = true"
+						>
+							References
+						</p>
+					</RouterLink>
+					<RouterLink :to="{ name: 'zevtera-prescribing-information', query: { navigatedAwayBy: 'sidebar' } }">
+						<p
+							class="text-xl font-uni-grotesk text-[#969696]"
+							@click="prescribingPopupOpen = true"
+						>
+							Prescribing information
+						</p>
+					</RouterLink>
+				</div>
+				<img
+					ref="advanzLargeLogo"
+					src="/src/assets/images/advanz-logo.png"
+					alt="Advanz Logo"
+					class="w-44 h-auto m-auto"
+				/>
+				<img
+					ref="advanzSmallLogo"
+					src="/src/assets/images/advanz-logo-small.png"
+					alt="Advanz Logo"
+					class="w-12 h-auto m-auto"
+				/>
+			</div>
+		</aside>
+	</div>
 </template>
 
 <script setup>
@@ -204,8 +220,8 @@ import { ref, onMounted } from 'vue';
 
 import { gsap } from 'gsap';
 
-import SimpleChevronLeftIcon from '@/icons/SimpleChevronLeftIcon.vue';
-import SimpleChevronRightIcon from '@/icons/SimpleChevronRightIcon.vue';
+import SimpleChevronLeftIcon from '../../icons/SimpleChevronLeftIcon.vue';
+import SimpleChevronRightIcon from '../../icons/SimpleChevronRightIcon.vue';
 
 const props = defineProps({
 	open: {
@@ -220,10 +236,28 @@ const route = useRoute();
 const referencesPopupOpen = ref(false);
 const prescribingPopupOpen = ref(false);
 
+const sidebar = ref(null);
+const bottomText = ref(null);
+const sidebarLine = ref(null);
+const bottomIcons = ref(null);
+const advanzLargeLogo = ref(null);
+const advanzSmallLogo = ref(null);
+
 onMounted(() => {
 	gsap.set('.sidebar-text', {
 		opacity: 1,
 		display: 'block',
+	});
+	gsap.set(sidebar.value, {
+		width: 350,
+		marginTop: 32,
+	});
+	gsap.set(advanzSmallLogo.value, {
+		opacity: 0,
+		display: 'none',
+	});
+	gsap.set('.list-image', {
+		left: 12,
 	});
 });
 
@@ -233,18 +267,103 @@ const isActive = (currentRoute) => {
 
 const toggleSidebar = async () => {
 	if (props.open) {
-		gsap.set('.sidebar-text', {
+		gsap.to('.sidebar-text', {
 			opacity: 0,
 			display: 'none',
+			duration: 0.5,
 		});
+		gsap.to(sidebarLine.value, {
+			opacity: 0,
+			duration: 0.5,
+		});
+		gsap.to(bottomText.value, {
+			opacity: 0,
+			display: 'none',
+			duration: 0.5,
+		});
+		gsap.to(bottomIcons.value, {
+			opacity: 0,
+			duration: 0.5,
+		});
+		gsap.to(bottomIcons.value, {
+			opacity: 1,
+			gridTemplateColumns: '1fr',
+			duration: 0.5,
+		}).delay(0.6);
+		gsap.to(advanzLargeLogo.value, {
+			opacity: 0,
+			display: 'none',
+			duration: 0.5,
+		});
+		gsap.to(advanzSmallLogo.value, {
+			opacity: 1,
+			display: 'block',
+			duration: 0.5,
+		}).delay(0.6);
+		gsap.to(sidebar.value, {
+			width: 118,
+			clipPath: 'inset(15% 0 0 0)',
+			ease: 'power4.inOut',
+			duration: 0.5,
+		});
+		gsap.to('.list-image', {
+			left: '50%',
+			translateX: '-50%',
+			duration: 0.1,
+		}).delay(0.55);
+
 		emit('update:open', false);
 	} else {
 		emit('update:open', true);
+
+		gsap.to(sidebar.value, {
+			width: 350,
+			marginTop: 32,
+			clipPath: 'inset(0 0 0 0)',
+			ease: 'power4.inOut',
+			duration: 0.4,
+		});
+		gsap.to(sidebar.value, {
+			clipPath: 'none',
+		}).delay(0.5);
 		gsap.to('.sidebar-text', {
 			opacity: 1,
 			display: 'block',
-			duration: 0.4,
-		}).delay(0.3);
+			duration: 0.3,
+		}).delay(0.6);
+		gsap.to(bottomText.value, {
+			opacity: 1,
+			display: 'grid',
+			duration: 0.3,
+		}).delay(0.6);
+		gsap.to(bottomIcons.value, {
+			opacity: 0,
+			duration: 0.3,
+		});
+		gsap.to(bottomIcons.value, {
+			opacity: 1,
+			gridTemplateColumns: '1fr 1fr',
+			duration: 0.3,
+		}).delay(0.6);
+		gsap.to(advanzSmallLogo.value, {
+			opacity: 0,
+			display: 'none',
+			duration: 0.3,
+		});
+		gsap.to(advanzLargeLogo.value, {
+			opacity: 1,
+			display: 'block',
+			duration: 0.3,
+		}).delay(0.6);
+		gsap.to('.list-image', {
+			left: 12,
+			translateX: 0,
+			duration: 0.1,
+		}).delay(0.1);
+		gsap.to(sidebarLine.value, {
+			opacity: 1,
+			duration: 0.3,
+		}).delay(0.6);
 	}
 };
 </script>
