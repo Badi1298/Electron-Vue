f85bfd91dba0b52a433e9c8360d7ef2ea651ee81
Badi1298 <serbandavid83@yahoo.com>
2025-03-19 16:26:09 +0200

    feat: improve layout and styling of Summary page for better readability and visual consistency

--- src/pages/xyd/Summary.vue
+++ src/pages/xyd/Summary.vue
@@ -5,8 +5,8 @@
 			alt="Summary BG"
 			class="h-screen w-fit rounded-r-[20px]"
 		/>
-		<div class="relative flex flex-col justify-center">
-			<section class="flex-1 content-center ml-9">
+		<div class="relative flex flex-col flex-1 px-9 pb-10">
+			<section class="flex flex-col flex-1 justify-end mb-20">
 				<div class="flex flex-col">
 					<div class="flex items-center gap-x-10">
 						<div class="relative flex items-center justify-center border-[3px] border-primary-purple rounded-full w-[100px] h-[100px]">
@@ -15,11 +15,11 @@
 						</div>
 						<div class="relative flex flex-col">
 							<h2 class="text-[40px] text-primary-purple font-bold">1 dose of Xydalba™</h2>
-							<div class="flex gap-x-3 absolute top-16 left-0 w-[900px] text-[20px] text-primary-purple leading-tight">
+							<div class="flex gap-x-3 absolute top-16 left-0 w-[900px] text-primary-purple leading-tight">
 								<img
 									src="/src/assets/images/list-item-emblem-purple.png"
 									alt="List Emblem"
-									class="h-[15px] translate-y-2"
+									class="h-[15px] translate-y-[3px]"
 								/>
 								<p>
 									A single dose of Xydalba™ provides a simple and full treatment course, freeing your<br />
@@ -37,27 +37,29 @@
 						</div>
 						<div class="relative flex flex-col">
 							<h2 class="text-[40px] text-primary-purple font-bold">2 weeks of effective treatment</h2>
-							<div class="flex gap-x-3 absolute top-16 left-0 w-[900px] text-[20px] text-primary-purple leading-tight">
+							<div class="flex gap-x-6 absolute top-16 left-0 w-[900px] mt-4 text-primary-purple leading-tight">
 								<div class="flex gap-x-3">
 									<img
 										src="/src/assets/images/list-item-emblem-purple.png"
 										alt="List Emblem"
-										class="h-[15px] translate-y-2"
+										class="h-[15px] translate-y-[3px]"
 									/>
 									<p>
 										Potent activity against key<br />
-										Gram-positive pathogens, including multi-resistant strains<sup>11</sup>
+										Gram-positive pathogens,<br />
+										including multi-resistant strains<sup>11</sup>
 									</p>
 								</div>
 								<div class="flex gap-x-3">
 									<img
 										src="/src/assets/images/list-item-emblem-purple.png"
 										alt="List Emblem"
-										class="h-[15px] translate-y-2"
+										class="h-[15px] translate-y-[3px]"
 									/>
 									<p>
 										Xydalba™ delivers long-lasting treatment without<br />
-										compromising on early clinical response, and demonstrated effectiveness in paediatric patients<sup>5,8</sup>
+										compromising on early clinical response, and demonstrated<br />
+										effectiveness in paediatric patients<sup>5,8</sup>
 									</p>
 								</div>
 							</div>
@@ -68,16 +70,16 @@
 					<div class="flex items-center gap-x-10">
 						<div class="relative flex items-center justify-center border-[3px] border-primary-turqoise rounded-full w-[100px] h-[100px]">
 							<span class="text-[52px] translate-x-1 text-primary-turqoise font-bold">3.</span>
-							<div class="absolute -bottom-52 w-[3px] h-[190px] bg-gradient-to-b from-primary-turqoise to-primary-purple rounded-full"></div>
+							<div class="absolute -bottom-40 w-[3px] h-[144px] bg-gradient-to-b from-primary-turqoise to-primary-purple rounded-full"></div>
 						</div>
 						<div class="relative flex flex-col">
 							<h2 class="text-[40px] text-primary-purple font-bold">30-minute infusion*1</h2>
-							<div class="flex flex-col gap-y-6 absolute top-16 left-0 w-[900px] text-[20px] text-primary-purple leading-tight">
+							<div class="flex flex-col gap-y-6 absolute top-16 left-0 w-[900px] text-primary-purple leading-tight mt-4">
 								<div class="flex gap-x-3">
 									<img
 										src="/src/assets/images/list-item-emblem-purple.png"
 										alt="List Emblem"
-										class="h-[15px] translate-y-2"
+										class="h-[15px] translate-y-[3px]"
 									/>
 									<p>
 										No dose adjustment, except for severe renal impairment<sup>†</sup>, and for infants and children (weight-based)<sup
@@ -89,17 +91,18 @@
 									<img
 										src="/src/assets/images/list-item-emblem-purple.png"
 										alt="List Emblem"
-										class="h-[15px] translate-y-2"
+										class="h-[15px] translate-y-[3px]"
 									/>
 									<p>
-										No required TDM (Therapeutic Drug Monitoring), or blood cell, or CPK (creatinine phosphokinase) monitoring<sup>1</sup>
+										No required TDM (Therapeutic Drug Monitoring), or blood cell, or CPK (creatinine phosphokinase)<br />
+										monitoring<sup>1</sup>
 									</p>
 								</div>
 								<div class="flex gap-x-3">
 									<img
 										src="/src/assets/images/list-item-emblem-purple.png"
 										alt="List Emblem"
-										class="h-[15px] translate-y-2"
+										class="h-[15px] translate-y-[3px]"
 									/>
 									<p>Low potential for drug-drug interactions<sup>‡1</sup></p>
 								</div>
@@ -107,7 +110,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="flex items-center gap-x-10 mt-[220px]">
+				<div class="flex items-center gap-x-10 mt-[180px]">
 					<img
 						src="/src/assets/images/xyd-summary-bed.png"
 						alt="Bed Icon"
@@ -116,13 +119,13 @@
 					<h2 class="text-[40px] text-primary-purple font-bold">Less days in hospital<sup>2,3</sup></h2>
 				</div>
 			</section>
-			<footer class="flex justify-between absolute bottom-10 left-9 w-[1000px]">
+			<footer class="flex justify-between gap-x-12 items-center">
 				<the-footer class="flex-1"
-					>*Clinical success achieved in 84% of adult patients (ITT) on day 14 in the phase 3 study assessing a single dose vs 2-dose<br />
-					regimen of dalbavacin in ABSSSI.<sup>4</sup> In paediatric patients, clinical cure rate similar across dalbavancin Single-Dose (97.4%)<br />
-					and 2-dose regimen (97.3%).<sup>1 †</sup>Caution should be exercised when prescribing Xydalba™ to patients with moderate or severe<br />
-					hepatic impairment (Child-Pugh Class B or C) as no data are available to determine the appropriate dosing in these<br />
-					patients.<sup>1 ‡</sup>Clinical drug-drug interaction studies with dalbavancin have not been conducted.<sup>1</sup></the-footer
+					>*Clinical success achieved in 84% of adult patients (ITT) on day 14 in the phase 3 study assessing a single dose vs 2-dose regimen of
+					dalbavacin in ABSSSI.<sup>4</sup> In paediatric patients, clinical cure rate similar across dalbavancin Single-Dose (97.4%) and 2-dose
+					regimen (97.3%).<sup>1 †</sup>Caution should be exercised when prescribing Xydalba™ to patients with moderate or severe hepatic impairment
+					(Child-Pugh Class B or C) as no data are available to determine the appropriate dosing in these patients.<sup>1 ‡</sup>Clinical drug-drug
+					interaction studies with dalbavancin have not been conducted.<sup>1</sup></the-footer
 				>
 				<explore-another-short />
 			</footer>
