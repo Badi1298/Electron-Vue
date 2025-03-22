05976b24490db5d9feb31c9cc82547e56154972a
Badi1298 <serbandavid83@yahoo.com>
2025-03-22 12:02:38 +0200

    fix: update ExploreAnotherShort component layout for improved styling and alignment

--- src/components/ExploreAnotherShort.vue
+++ src/components/ExploreAnotherShort.vue
@@ -1,10 +1,10 @@
 <template>
 	<RouterLink :to="{ name: 'home', query: { navigatedAwayBy: 'explore-another-button' } }">
-		<div class="flex items-center shadow-explore font-futura">
+		<div class="flex items-center shadow-explore font-futura rounded-r-[20px]">
 			<div class="flex items-center py-6 pl-10 pr-[46px] rounded-l-[20px] bg-[#195C68] h-[70px] relative z-10 text-white text-base shadow-explore">
 				Explore another brand
 			</div>
-			<div class="bg-white px-[17px] -translate-x-3 flex justify-end shadow-explore rounded-[20px] py-2.5 relative z-20">
+			<div class="bg-white px-[17px] -ml-3 flex justify-end shadow-explore rounded-[20px] py-2.5 relative z-20">
 				<img
 					src="/src/assets/images/chevron-right-green.png"
 					alt="Chevron Left Green"
